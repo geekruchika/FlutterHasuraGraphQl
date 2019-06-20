ef283ebfeb4bf464e97a1db7c2837c2be3382f84
Ruchika Gupta <ruchika@geekyants.com>
2019-06-20 11:36:52 +0530

    fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hasura_todo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TodoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
